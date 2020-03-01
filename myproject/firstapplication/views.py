254e4582843fc70bec3d60206724320ac80b9f35
himanshu9345 <hphimanshu6@gmail.com>
2020-03-01 16:58:43 -0500

    chnaged add number feature to fetch data from post

--- myproject/firstapplication/views.py
+++ myproject/firstapplication/views.py
@@ -6,7 +6,7 @@ def home(request):
     return render(request,"home.html",{"name":"Himanshu"})#passing dictionary for dynamic loading
 
 def add(request):
-    val1 = int(request.GET['num1'])
-    val2 = int(request.GET['num2'])
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
     result = val1+val2
     return render(request,'result.html',{'result':result})
