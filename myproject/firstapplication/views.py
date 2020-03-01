11b62ed80ea6a2f1fbd8e4c9e7f5cb83f14203c4
himanshu9345 <hphimanshu6@gmail.com>
2020-03-01 16:51:58 -0500

    adding 2 user entered number and displayed result

--- myproject/firstapplication/views.py
+++ myproject/firstapplication/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request,"home.html",{"name":"Himanshu"})#passing dictionary for dynamic loading
+
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    result = val1+val2
+    return render(request,'result.html',{'result':result})
