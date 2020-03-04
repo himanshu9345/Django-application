b0a094a36e196dff35738463889610cdeee8c10c
himanshu9345 <hphimanshu6@gmail.com>
2020-03-04 01:58:19 -0500

    created model and migrated it to postgresql

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travel.apps.TravelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST':'localhost'
+
     }
 }
 
--- myproject/travel/models.py
+++ myproject/travel/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Destination:
-    id: int
-    name: str
-    img: str
-    desc: str
-    price: str
-    offer: bool
+class Destination(models.Model):
+    name =models.CharField(max_length=200)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    price = models.ImageField()
+    offer = models.BooleanField(default=False)
