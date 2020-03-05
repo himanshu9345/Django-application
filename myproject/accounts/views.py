3ff431ad94b124f115504cbe971178a132be3688
himanshu9345 <hphimanshu6@gmail.com>
2020-03-04 22:28:39 -0500

    user registration with validation

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
 
 # Create your views here.
 def register(request):
-    return render(request,"register.html")
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if password2==password1:
+            if User.objects.filter(username=username).exists():
+                print("username taken")
+            elif User.objects.filter(email=email).exists():
+                print("email exists")
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password1,first_name=first_name,last_name=last_name)
+                user.save()
+                print("user created")
+        else:
+            print("password dont match")
+        return redirect('/travel')
+
+    else:
+        return render(request,"register.html")
