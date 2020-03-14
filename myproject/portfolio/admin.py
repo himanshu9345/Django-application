7c469750ba4bcdaf79be21a5f5747ed69da2e134
himanshu9345 <hphimanshu6@gmail.com>
2020-03-13 23:03:21 -0400

    added listdisplay in admin, made project clickable

--- myproject/portfolio/admin.py
+++ myproject/portfolio/admin.py
@@ -1,10 +1,25 @@
 from django.contrib import admin
 from .models import Experience,Skill,Award,Publication,Project
 # Register your models here.
-admin.site.register(Experience)
-admin.site.register(Skill)
-admin.site.register(Award)
-admin.site.register(Publication)
-admin.site.register(Project)
 
 
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = [field.attname for field in Experience._meta.fields]
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = [field.attname for field in Skill._meta.fields]
+
+class AwardAdmin(admin.ModelAdmin):
+    list_display = [field.attname for field in Award._meta.fields]
+
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = [field.attname for field in Publication._meta.fields]
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = [field.attname for field in Project._meta.fields]
+
+admin.site.register(Experience,ExperienceAdmin)
+admin.site.register(Skill,SkillAdmin)
+admin.site.register(Award,AwardAdmin)
+admin.site.register(Publication,PublicationAdmin)
+admin.site.register(Project,ProjectAdmin)
