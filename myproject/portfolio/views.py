bde8598145ee56ef28c4f840f5524d2bbcc33fee
himanshu9345 <hphimanshu6@gmail.com>
2020-03-15 20:17:45 -0400

    added email functionality

--- myproject/portfolio/views.py
+++ myproject/portfolio/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render,redirect
 from .models import Experience,Skill,Award,Publication,Project
 from django.core.mail import send_mail
 from django.conf import settings
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from dotenv import load_dotenv
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
@@ -29,17 +35,44 @@ def sendemail(request):
         email=request.POST['email']
         email_subject=request.POST['email_subject']
         message=request.POST['message']
-        print(message,email,person_name,email_subject)
+        # print(message,email,person_name,email_subject)
 
-        to_sender="Hello "+person_name+",\n"+"you will be contacted soon\n"+"Your Message was, "+message
+        to_sender="Hello "+person_name+",\n"+"you will be contacted soon. <br>"+"Your Message was, <strong>"+message+"<strong>"
         to_sender_subject="Replay from Himanshu"
-        #requester
-        send_mail(to_sender_subject,to_sender,settings.EMAIL_HOST_USER1,[settings.EMAIL_HOST_USER1],fail_silently=False,)
+        # #requester
+        # # send_mail(to_sender_subject,to_sender,settings.EMAIL_HOST_USER1,[settings.EMAIL_HOST_USER1],fail_silently=False,)
         
-        #myself
-        to_me="Hello Himanshu,\n"+"you got contacted by "+person_name+" \n"+"Message for you is, "+message
-        # send_mail(email_subject,to_me,settings.EMAIL_HOST_USER1,['hbp53@njit.edu'],fail_silently=False,)
-        return redirect('index')
-    else:
-        return redirect('index')
+        # #myself
+        to_me="Hello Himanshu,\n"+"you got contacted by "+person_name+" <br>"+"Message for you is, <strong>"+message+"<strong>"
 
+        
+
+        message1 = Mail(
+            from_email='faltu9345@gmail.com',
+            to_emails='hbp53@njit.edu',
+            subject=email_subject,
+            html_content=to_me)
+        message2 = Mail(
+            from_email='faltu9345@gmail.com',
+            to_emails=email,
+            subject=to_sender_subject,
+            html_content=to_sender)
+        try:
+            
+            sg = SendGridAPIClient(os.getenv('SEND_GRID_KEY'))
+            response = sg.send(message1)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+            response = sg.send(message2)
+            print(response.status_code)
+            print(response.body)
+            print(response.headers)
+        except Exception as e:
+            print(e)
+            print("nope")
+
+        return render(request,'index.html')
+    else:
+        
+        return render(request,'index.html')
