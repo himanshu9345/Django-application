3543d154f105aaa9c5ca8cf31075f4bd94edd670
himanshu9345 <hphimanshu6@gmail.com>
2020-03-19 00:38:21 -0400

    image renaming function

--- myproject/portfolio/models.py
+++ myproject/portfolio/models.py
@@ -1,20 +1,30 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-import os
+import os,time
 from uuid import uuid4
+import uuid
+from django.utils.deconstruct import deconstructible
+
 # Create your models here.
-def path_and_rename(path):
-    def wrapper(instance, filename):
-        ext = filename.split('.')[-1]
-        # get filename
-        if instance.pk:
-            filename = '{}.{}'.format(instance.pk, ext)
-        else:
-            # set filename as random string
-            filename = '{}.{}'.format(uuid4().hex, ext)
-        # return the whole path to the file
-        return os.path.join(path, filename)
-    return wrapper
+
+@deconstructible
+class PathAndRename(object):
+    def __init__(self, sub_path):
+        self.path = sub_path
+
+    def __call__(self, instance, filename):
+        # eg: filename = 'my uploaded file.jpg'
+        ext = filename.split('.')[-1]  #eg: 'jpg'
+        uid = uuid.uuid4().hex[:10]    #eg: '567ae32f97'
+
+        # eg: 'my-uploaded-file'
+        new_name = '-'.join(filename.replace('.%s' % ext, '').split())
+
+        # eg: 'my-uploaded-file_64c942aa64.jpg'
+        renamed_filename = '%(new_name)s_%(uid)s.%(ext)s' % {'new_name': new_name, 'uid': uid, 'ext': ext}
+
+        # eg: 'images/2017/01/29/my-uploaded-file_64c942aa64.jpg'
+        return os.path.join(self.path, renamed_filename)
 
 class Experience(models.Model):
     company = models.TextField()
@@ -44,9 +54,12 @@ class Publication(models.Model):
     publication_description = models.TextField()
 
 class Project(models.Model):
+    
     project_title = models.CharField(max_length=200)
     project_description = models.TextField()
     project_url = models.URLField()
-    project_photo = models.ImageField(upload_to=path_and_rename('pics'))
+    image_path = time.strftime('pics/%Y/%m/%d')
+    project_photo = models.ImageField(upload_to=PathAndRename(image_path))
     project_start_year = models.CharField(max_length=4)
     project_end_year =models.CharField(max_length=4)
+    
