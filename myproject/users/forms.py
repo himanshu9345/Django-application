13c1cbf6ea710d066a41e54ace8a98bc65c44865
himanshu9345 <hphimanshu6@gmail.com>
2020-03-21 15:55:32 -0400

    able to load user and userextra details on profile

--- myproject/users/forms.py
+++ myproject/users/forms.py
@@ -1,8 +1,10 @@
 from django import forms
 from .models import UserExtraDetails
+from django.contrib.auth.models import User
 from django.forms import ModelForm
+from django.contrib.auth.forms import UserChangeForm
 
-class UserForm(ModelForm):
+class UserExtraDetailsForm(ModelForm):
     
     class Meta:
         model=UserExtraDetails
@@ -10,9 +12,9 @@ class UserForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['user_interest'].widget.attrs.update({'class': 'form-control'})
-        self.fields['user_address'].widget.attrs.update({'class': 'form-control'})
-        self.fields['user_project_completed'].widget.attrs.update({'class': 'form-control'})
+        self.fields['user_interest'].widget.attrs.update({'class': 'form-control','onClick':'myFunction()','readonly':'readonly','oninput':'checkInputChanged(id_user_interest)'})
+        self.fields['user_address'].widget.attrs.update({'class': 'form-control','onClick':'myFunction()','readonly':'readonly','oninput':'checkInputChanged(id_user_address)'})
+        self.fields['user_project_completed'].widget.attrs.update({'class': 'form-control','onClick':'myFunction()','readonly':'readonly','oninput':'checkInputChanged(id_user_project_completed)'})
 
     def save(self, user,id):
         obj = super().save(commit = False)
@@ -20,3 +22,17 @@ class UserForm(ModelForm):
         obj.id=id
         obj.save()
         return obj
+
+class UserForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','username', 'email' )
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['first_name'].widget.attrs.update({'class': 'form-control','onClick':'myFunction()','readonly':'readonly','oninput':'checkInputChanged(id_first_name)'})
+        self.fields['last_name'].widget.attrs.update({'class': 'form-control','onClick':'myFunction()','readonly':'readonly','oninput':'checkInputChanged(id_last_name)'})
+        self.fields['username'].widget.attrs.update({'class': 'form-control','onClick':'myFunction()','readonly':'readonly','oninput':'checkInputChanged(id_username)'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control','onClick':'myFunction()','readonly':'readonly','oninput':'checkInputChanged(id_email)'})
+    
--- myproject/users/views.py
+++ myproject/users/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from .forms import UserForm
+from django.shortcuts import render,redirect
+from .forms import UserExtraDetailsForm,UserForm
 from django.contrib.auth.decorators import login_required
 from .models import UserExtraDetails
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
 # Create your views here.
 
 def viewprofile(request,user_id):
@@ -14,16 +18,23 @@ def showEditableProfile(request):
     current_user_id=request.user.id
 
     if request.method=="POST":
-        form = UserForm(request.POST)
+        form = UserExtraDetailsForm(request.POST)
+        user_form=UserForm(data=request.POST,instance=request.user)
+        print(user_form.fields.keys())
+
         if form.is_valid():
-            print("valid Form")
             profile_id=UserExtraDetails.objects.get(user_id=current_user_id)
             form.save(request.user,profile_id.id)
-            
-            # form.user.add(*[request.user])
-        print(form)
-        # user_ins=user_extra_details.cleaned_data['user_intrest']
-        # print (form.)
+        if user_form.is_valid():
+            if User.objects.filter(username=user_form.cleaned_data['username']).exists():
+                messages.info(request,"Username taken")
+                return redirect('profile')
+            elif User.objects.filter(email=user_form.cleaned_data['email']).exists():
+                messages.info(request,"Email exists")
+                return redirect('profile')
+            else:
+                print("valid user from")
+                user_form.save()
         return render(request,"profile.html",{'user_details_form':form})
 
 
@@ -31,8 +42,9 @@ def showEditableProfile(request):
     current_user_id=request.user.id
     print(type(current_user_id))
     user_extra_details=UserExtraDetails.objects.get(user_id=current_user_id)
+    user_info=User.objects.get(id=current_user_id)
 
-    print(user_extra_details)
-    user_details_form=UserForm(initial={'user_interest':user_extra_details.user_interest,'user_address':user_extra_details.user_address,'user_project_completed':user_extra_details.user_project_completed})
-    
-    return render(request,"profile.html",{'user_details_form':user_details_form})
+    print(user_extra_details,type(user_info))
+    user_details_form=UserExtraDetailsForm(initial={'user_interest':user_extra_details.user_interest,'user_address':user_extra_details.user_address,'user_project_completed':user_extra_details.user_project_completed})
+    user_info_form=UserForm(initial={'first_name':user_info.first_name,'last_name':user_info.last_name,'username':user_info.username,'email':user_info.email})
+    return render(request,"profile.html",{'user_details_form':user_details_form,'user_info_form':user_info_form})
