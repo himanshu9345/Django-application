00c524f4fa0001e7131ea2a2c66646b44ef1abbd
himanshu9345 <hphimanshu6@gmail.com>
2020-11-22 14:11:23 -0500

    Moved project to Azure

--- custom_storages.py
+++ custom_storages.py
@@ -1,8 +1,14 @@
 from django.conf import settings
-from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.azure_storage import AzureStorage
 
-class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+class StaticStorage(AzureStorage):
+    account_name = 'buildmyportfolio' # Must be replaced by your <storage_account_name>
+    account_key = 'pznNUsPouNWrFBoOHBVYidqDqwmrMfwAtddXhW86QLSADAGLwvl8tTVsoSaeFxo/s7GCgT8N+jFOCsXCRP894w==' # Must be replaced by your <storage_account_key>
+    azure_container = 'sitedata/static'
+    expiration_secs = None
 
-class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+class MediaStorage(AzureStorage):
+    account_name = 'buildmyportfolio' # Must be replaced by your <storage_account_name>
+    account_key = 'pznNUsPouNWrFBoOHBVYidqDqwmrMfwAtddXhW86QLSADAGLwvl8tTVsoSaeFxo/s7GCgT8N+jFOCsXCRP894w==' # Must be replaced by your <storage_account_key>
+    azure_container = 'sitedata/media'
+    expiration_secs = None
